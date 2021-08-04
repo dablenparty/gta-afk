009371c3c43a81105799c2039d40cd8e9c76f91c
dablenparty <hdavenport8902@gmail.com>
2021-08-04 18:42:58 -0400

    Implemented holding multiple keys

--- gta-afk/Program.cs
+++ gta-afk/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
 using WindowsInput;
 using WindowsInput.Native;
 
@@ -7,23 +8,38 @@ namespace gta_afk
 {
     class Program
     {
+        private static readonly Random RandomInstance = new();
+        private static readonly InputSimulator InputSimulatorInstance = new();
+        
         /// <summary>
         /// Holds a key for a specified amount of time
         /// </summary>
         /// <param name="keyCode">Key to hold</param>
-        /// <param name="simulator">The input simulator</param>
         /// <param name="timeoutInMillis">Time (in milliseconds) to hold the key for</param>
-        private static void HoldKey(VirtualKeyCode keyCode, IInputSimulator simulator, int timeoutInMillis)
+        private static void HoldKey(VirtualKeyCode keyCode, int timeoutInMillis)
         {
-            simulator.Keyboard.KeyDown(keyCode);
-            simulator.Keyboard.Sleep(timeoutInMillis);
-            simulator.Keyboard.KeyUp(keyCode);
+            Console.WriteLine($"Holding {keyCode} for {timeoutInMillis} milliseconds");
+            InputSimulatorInstance.Keyboard.KeyDown(keyCode);
+            InputSimulatorInstance.Keyboard.Sleep(timeoutInMillis);
+            InputSimulatorInstance.Keyboard.KeyUp(keyCode);
+        }
+
+        /// <summary>
+        /// Holds multiple keys for a specified amount of time
+        /// </summary>
+        /// <param name="keyCodesEnumerable">Keys to hold</param>
+        /// <param name="timeoutInMillis">Time (in milliseconds) to hold the keys for</param>
+        private static void HoldKeys(IEnumerable<VirtualKeyCode> keyCodesEnumerable, int timeoutInMillis)
+        {
+            var keyCodesArray = keyCodesEnumerable as VirtualKeyCode[] ?? keyCodesEnumerable.ToArray();
+            Console.WriteLine($"Holding {keyCodesArray} for {timeoutInMillis} milliseconds");
+            foreach (var keyCode in keyCodesArray) InputSimulatorInstance.Keyboard.KeyDown(keyCode);
+            InputSimulatorInstance.Keyboard.Sleep(timeoutInMillis);
+            foreach (var keyCode in keyCodesArray) InputSimulatorInstance.Keyboard.KeyUp(keyCode);
         }
         
         public static void Main(string[] args)
         {
-            var inputSimulator = new InputSimulator();
-            var random = new Random();
             VirtualKeyCode[] movementKeys =
             {
                 VirtualKeyCode.VK_W, 
@@ -36,15 +52,38 @@ namespace gta_afk
             var handle = User32Dll.FindWindow(null, windowName);
             if (handle != IntPtr.Zero && User32Dll.SetForegroundWindow(handle))
             {
-                var key = movementKeys[random.Next(0, movementKeys.Length)];
-                var delay = random.Next(2, 8) * 500; // random delay between 1 and 4 seconds in half second intervals
-                Console.WriteLine($"Pressing {key} for {delay} milliseconds");
-                HoldKey(key, inputSimulator, delay);
+                var delay = RandomInstance.Next(2, 8) * 500; // random delay between 1 and 4 seconds in half second intervals
+                var holdMultiple = RandomInstance.Next(0, 1) == 1;
+                if (holdMultiple)
+                {
+                    var keyCodes = new VirtualKeyCode[2];
+                    for (var i = 0; i < keyCodes.Length; i++)
+                    {
+                        var key = movementKeys[RandomInstance.Next(0, movementKeys.Length)];
+                        while (keyCodes.Contains(key))
+                        {
+                            key = movementKeys[RandomInstance.Next(0, movementKeys.Length)]; // don't hold the same key twice
+                        }
+                        
+                        keyCodes[i] = key;
+                    }
+                    HoldKeys(keyCodes, delay);
+                }
+                else
+                {
+                    var key = GetRandomMovementKey(movementKeys);
+                    HoldKey(key, delay);
+                }
             }
             else
             {
                 Console.WriteLine($"Window could not be set to foreground (handle: {handle})");
             }
+        }
+
+        private static VirtualKeyCode GetRandomMovementKey(IReadOnlyList<VirtualKeyCode> movementKeys)
+        {
+            return movementKeys[RandomInstance.Next(0, movementKeys.Count)];
         }
     }
 }
