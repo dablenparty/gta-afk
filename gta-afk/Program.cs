b936bdc8ec5dacf7222a45631be35b028385baf3
dablenparty <hdavenport8902@gmail.com>
2021-08-04 18:02:36 -0400

    Added InputSimulator

--- gta-afk/Program.cs
+++ gta-afk/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using WindowsInput;
+using WindowsInput.Native;
 
 namespace gta_afk
 {
@@ -6,7 +8,16 @@ namespace gta_afk
     {
         public static void Main(string[] args)
         {
-            const string windowClassName = "Notepad";
+            var inputSimulator = new InputSimulator();
+            VirtualKeyCode[] movementKeys =
+            {
+                VirtualKeyCode.VK_W, 
+                VirtualKeyCode.VK_A, 
+                VirtualKeyCode.VK_S, 
+                VirtualKeyCode.VK_D,
+                VirtualKeyCode.SPACE
+            };
+            const string windowClassName = "Grand Theft Auto V";
             var handle = User32Dll.FindWindow(windowClassName, null);
             if (handle != IntPtr.Zero && User32Dll.SetForegroundWindow(handle))
             {
