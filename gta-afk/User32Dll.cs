a582832c76b9ab4448c1176dda47f9cca08e1185
dablenparty <hdavenport8902@gmail.com>
2021-08-04 17:54:38 -0400

    Imported SetForegroundWindow and FindWindow from user32.dll

--- gta-afk/User32Dll.cs
+++ gta-afk/User32Dll.cs
@@ -1,7 +1,14 @@
+using System;
+using System.Runtime.InteropServices;
+
 namespace gta_afk
 {
     public class User32Dll
     {
-        
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
     }
 }
