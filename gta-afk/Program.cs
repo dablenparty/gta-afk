72162fa8216f04e0f1d376576f48a157ed849854
dablenparty <hdavenport8902@gmail.com>
2021-08-04 16:51:02 -0400

    Initial commit

---
+++ gta-afk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gta_afk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
