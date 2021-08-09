155929af7b15e3a51f96740452f8722103f71d4d
dablenparty <hdavenport8902@gmail.com>
2021-08-09 14:07:55 -0400

    Created HoldableVirtualKeyCode struct

---
+++ GtaAfk/HoldableVirtualKeyCode.cs
@@ -0,0 +1,16 @@
+using WindowsInput.Native;
+
+namespace GtaAfk
+{
+    public struct HoldableVirtualKeyCode
+    {
+        public VirtualKeyCode KeyCode { get; }
+        public bool Hold { get; }
+
+        public HoldableVirtualKeyCode(VirtualKeyCode keyCode, bool hold)
+        {
+            KeyCode = keyCode;
+            Hold = hold;
+        }
+    }
+}
