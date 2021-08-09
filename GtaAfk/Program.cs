8f521e5a9f99695260fd262efc4ef3916fd2e1e6
dablenparty <hdavenport8902@gmail.com>
2021-08-09 18:11:58 -0400

    Polish pass for output grammar, formatting, general code formatting, etc.

--- GtaAfk/Program.cs
+++ GtaAfk/Program.cs
@@ -11,15 +11,16 @@ namespace GtaAfk
     {
         private static readonly Random RandomInstance = new();
         private static readonly InputSimulator InputSimulatorInstance = new();
+
         private static readonly VirtualKeyCode[] MovementKeys =
         {
-            VirtualKeyCode.VK_W, 
-            VirtualKeyCode.VK_A, 
-            VirtualKeyCode.VK_S, 
+            VirtualKeyCode.VK_W,
+            VirtualKeyCode.VK_A,
+            VirtualKeyCode.VK_S,
             VirtualKeyCode.VK_D,
             VirtualKeyCode.LSHIFT
         };
-        
+
         /// <summary>
         /// Holds a key for a specified amount of time
         /// </summary>
@@ -27,7 +28,7 @@ namespace GtaAfk
         /// <param name="timeoutInMillis">Time (in milliseconds) to hold the key for</param>
         private static void HoldKey(VirtualKeyCode keyCode, int timeoutInMillis)
         {
-            HoldKey(new []{keyCode}, timeoutInMillis);
+            HoldKey(new[] {keyCode}, timeoutInMillis);
         }
 
         /// <summary>
@@ -37,42 +38,51 @@ namespace GtaAfk
         /// <param name="timeoutInMillis">Time (in milliseconds) to hold the keys for</param>
         private static void HoldKey(IEnumerable<VirtualKeyCode> keyCodesEnumerable, int timeoutInMillis)
         {
-            var keyCodesArray = keyCodesEnumerable as VirtualKeyCode[] ?? keyCodesEnumerable.ToArray();
-            Console.WriteLine($"Holding {string.Join(", ", keyCodesArray)} for {timeoutInMillis} milliseconds");
+            VirtualKeyCode[] keyCodesArray = keyCodesEnumerable as VirtualKeyCode[] ?? keyCodesEnumerable.ToArray();
+            var tail = timeoutInMillis == 1000 ? "second" : "seconds";
+            Console.Write(
+                $"\rHolding {string.Join(", ", keyCodesArray)} for {Convert.ToDecimal(timeoutInMillis) / 1000} {tail}"
+                    .PadRight(45));
             foreach (var keyCode in keyCodesArray) InputSimulatorInstance.Keyboard.KeyDown(keyCode);
             InputSimulatorInstance.Keyboard.Sleep(timeoutInMillis);
             foreach (var keyCode in keyCodesArray) InputSimulatorInstance.Keyboard.KeyUp(keyCode);
         }
-        
+
         public static void Main(string[] args)
         {
-            const string windowName = "Grand Theft Auto V";
-            var handle = User32Dll.FindWindow(null, windowName);
-            Console.WriteLine("Either close this window or click in and press Escape at any time to exit");
+            if (args.Length > 1) throw new ArgumentException($"Too many args");
+            var windowName = args.Length == 0 ? "Grand Theft Auto V" : args[0];
+            IntPtr windowHandle = User32Dll.FindWindow(null, windowName);
+            Console.WriteLine("Either close this window or click in and press Escape when it prompts you to exit");
             while (true)
             {
-                while (User32Dll.GetForegroundWindow() == handle)
+                while (User32Dll.GetForegroundWindow() == windowHandle)
                 {
-                    var delay = RandomInstance.Next(2, 9) * 500; // random delay between 1 and 4 seconds in half second intervals
+                    // random delay between 1 and 4 seconds in half second intervals
+                    var delay = RandomInstance.Next(2, 9) * 500;
                     var holdMultiple = RandomInstance.Next(0, 2) == 1;
                     if (holdMultiple)
                     {
-                        var keyCodes = GenerateMultipleRandomKeys(2);
+                        VirtualKeyCode[] keyCodes = GenerateMultipleRandomKeys(2);
                         // disallows opposite keys to be pressed so that movement is always ensured
-                        while ((keyCodes.Contains(VirtualKeyCode.VK_W) && keyCodes.Contains(VirtualKeyCode.VK_S)) 
-                            || (keyCodes.Contains(VirtualKeyCode.VK_A) && keyCodes.Contains(VirtualKeyCode.VK_D)))
+                        while (keyCodes.Contains(VirtualKeyCode.VK_W) && keyCodes.Contains(VirtualKeyCode.VK_S)
+                               || keyCodes.Contains(VirtualKeyCode.VK_A) && keyCodes.Contains(VirtualKeyCode.VK_D))
                         {
                             keyCodes = GenerateMultipleRandomKeys(2);
                         }
+
                         HoldKey(keyCodes, delay);
                     }
                     else
                     {
-                        var key = GetRandomMovementKey();
+                        VirtualKeyCode key = GetRandomMovementKey();
+                        // disallows left shift to be pressed on its own
+                        while (key == VirtualKeyCode.LSHIFT) key = GetRandomMovementKey();
                         HoldKey(key, delay);
                     }
                 }
 
+                Console.Write("\rPress Esc to exit".PadRight(45));
                 Thread.Sleep(500); // wait a half-second before checking again
                 if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape) break;
             }
@@ -83,7 +93,7 @@ namespace GtaAfk
             var keyCodes = new VirtualKeyCode[count];
             for (var i = 0; i < keyCodes.Length; i++)
             {
-                var key = GetRandomMovementKey();
+                VirtualKeyCode key = GetRandomMovementKey();
                 // don't use the same key twice
                 while (keyCodes.Contains(key)) key = GetRandomMovementKey();
 
